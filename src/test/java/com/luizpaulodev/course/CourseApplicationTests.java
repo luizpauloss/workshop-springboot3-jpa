197e8db7b632256a9f5ae3bd3035dfc10b365aad
Luiz Paulo <luizstyle@hotmail.com>
2024-04-05 10:19:21 -0300

    Projeto Criado

---
+++ src/test/java/com/luizpaulodev/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizpaulodev.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
