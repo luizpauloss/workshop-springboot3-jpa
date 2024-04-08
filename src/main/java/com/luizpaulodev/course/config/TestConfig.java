776edc80e427e6c5b2d956ae94d3d9b4f2000599
Luiz Paulo <luizstyle@hotmail.com>
2024-04-08 12:11:54 -0300

    Pagamento, associação individual

--- src/main/java/com/luizpaulodev/course/config/TestConfig.java
+++ src/main/java/com/luizpaulodev/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.luizpaulodev.course.entities.Category;
 import com.luizpaulodev.course.entities.Order;
 import com.luizpaulodev.course.entities.OrderItem;
+import com.luizpaulodev.course.entities.Payment;
 import com.luizpaulodev.course.entities.Product;
 import com.luizpaulodev.course.entities.User;
 import com.luizpaulodev.course.entities.enums.OrderStatus;
@@ -84,7 +85,10 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
 
-		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 	
 	
