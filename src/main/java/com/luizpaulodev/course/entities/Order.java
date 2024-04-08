f7ec6a6d18a5bae9d3e237312a10bf03b9864769
Luiz Paulo <luizstyle@hotmail.com>
2024-04-08 11:32:53 -0300

    OrdersStatus Enum

--- src/main/java/com/luizpaulodev/course/entities/Order.java
+++ src/main/java/com/luizpaulodev/course/entities/Order.java
@@ -3,6 +3,8 @@ package com.luizpaulodev.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.luizpaulodev.course.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,8 @@ public class Order implements Serializable {
 	private Long id;
 	private Instant moment;
 
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -28,11 +32,12 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
@@ -57,6 +62,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
