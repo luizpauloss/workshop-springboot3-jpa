3ce7fe33587828aca05f153da5444566e46beb94
Luiz Paulo <luizstyle@hotmail.com>
2024-04-08 12:27:46 -0300

    Tratamento de excecoes FindById

--- src/main/java/com/luizpaulodev/course/services/UserService.java
+++ src/main/java/com/luizpaulodev/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.luizpaulodev.course.entities.User;
 import com.luizpaulodev.course.repositories.UserRepository;
+import com.luizpaulodev.course.resources.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	
 	}
 	
